57f9c287da59d501a3dc0c9731cdeb3400bcaabc
Ramiz <LibraRamis@gmail.com>
2022-09-28 12:46:06 -0700

    Update routes.php

--- routes/routes.php
+++ routes/routes.php
@@ -1,10 +1,10 @@
 <?php
 
 use Slim\App;
-use App\Action\UserAction\GetAction;
+use App\Action\UserAction\GetUserAction;
 use PDO as DB;
 
 return function (App $app, DB $db) {
 
-    $app->get('/', GetAction::class);
+    $app->get('/', GetUserAction::class);
 };
