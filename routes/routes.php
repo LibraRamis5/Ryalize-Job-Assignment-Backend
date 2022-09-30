502efc7a369ed040a1ec8c085644af03133596d8
Ramiz <LibraRamis@gmail.com>
2022-09-30 10:56:40 -0700

    add update user api

--- routes/routes.php
+++ routes/routes.php
@@ -3,6 +3,7 @@
 use Slim\App;
 use App\Action\UserAction\GetUserAction;
 use App\Action\UserAction\AddUserAction;
+use App\Action\UserAction\UpdateUserAction;
 use App\Action\HomePage\HomePageAction;
 
 return function (App $app) {
@@ -13,5 +14,6 @@ return function (App $app) {
     // user routes
     $app->get('/user', GetUserAction::class);
     $app->post('/user-add', AddUserAction::class);
+    $app->put('/user-update', UpdateUserAction::class);
 
 };
