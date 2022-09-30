7c2734c068fbeed970ff440a34aabb79201af09a
Ramiz <LibraRamis@gmail.com>
2022-09-30 09:49:00 -0700

    add user action complete with validation

--- src/Action/Action.php
+++ src/Action/Action.php
@@ -3,27 +3,41 @@
 namespace App\Action;
 
 use Psr\Http\Message\ResponseInterface as Response;
+use Rakit\Validation\Validator;
+
 
 class Action{
 
     public function __construct()
-     {
-             
+    {
+        $this->validator = new Validator;
     }
 
-    public function success(Response $response,$data) {
-        return $this->responce($response,$data,200);
-    }   
+    protected function validate($data,$rules){
+        $validation = $this->validator->validate($data, $rules);
 
-    public function responce(Response $response,$data, $code) {
+        if ($validation->fails())
+        {
+            $error = $validation->errors()->firstOfAll();
+            return $error;
+        }
+
+        return false;
+    }
+
+    protected function success(Response $response,$data) {
+        return $this->responce($response,$data,200);
+    }
+
+    protected function responce(Response $response,$data, $code) {
         $response->getBody()->write(json_encode($data));
         return $response
-                ->withHeader('content-type', 'application/json')
-                ->withStatus($code);
-    }   
-    
-    public function error(Response $response,$data) {
-        $data = array( "message" => $data );        
+            ->withHeader('content-type', 'application/json')
+            ->withStatus($code);
+    }
+
+    protected function error(Response $response,$data) {
+        $data = array( "message" => $data );
         return $this->responce($response,$data,500);
-    }  
+    }
 }
--- src/Action/UserAction/AddUserAction.php
+++ src/Action/UserAction/AddUserAction.php
@@ -1,45 +1,50 @@
 <?php
-
 namespace App\Action\UserAction;
 
 use App\Action\Action;
+use Illuminate\Support\Facades\Hash;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use App\Models\User;
-use Respect\Validation\Validator as v;
 
-final class AddUserAction extends Action{
+final class AddUserAction extends Action
+{
+    public function __invoke(Request $request, Response $response): Response{
 
-    private function validator($data){
-        $error = [];
-        
-        if(isset($data['user_name']) || v::alnum()->noWhitespace()->length(1, 10)->validate($data['user_name']))
-            $error[] = "user name is requried";
-       
-        if(count($error) > 0){
-            return $error;
-        }
+        $data = (array)$request->getParsedBody();
 
-        return false;
-    }
+        $validation = $this->validate($data, [
+            'user_name' => 'required|regex:/^[a-zA-Z]+$/u|min:3|max:20',
+            'email' => 'required|email',
+            'phone_no' => 'required|regex:/^[0-9]/|max:12',
+            'address' => 'required|regex:/(^[-0-9A-Za-z.,\/ ]+$)/|max:50',
+            'password' =>  'required|min:8|regex:/[a-z]/|regex:/[A-Z]/|regex:/[0-9]/|regex:/[@$!%*#?&]/'
+        ]);
 
-    public function __invoke(Request $request, Response $response, $args): Response {
-        $data = $request->getParsedBody();       
-        
-
-        if($errors = $this->validator($data)){
-            return $this->error($response,$errors);
+        if($validation){
+            return $this->responce($response, $validation, 401);
         }
 
         try{
-            die('okay');
-            $users = User::all();
-            return $this->success($response,$users);
 
-        } catch (\Exception $e) {
-            return $this->error($response,$error);
-       }
+            if (User::where('email',$data['email'])->exists()) {
+                $email['message'] = 'Email already exists';
+                return $this->responce($response, $email, 409 );
+            }
+
+            $newUser = new User();
+            $newUser->user_name = $data['user_name'];
+            $newUser->email = $data['email'];
+            $newUser->password = $data['password'];
+            $newUser->phone_no = $data['phone_no'];
+            $newUser->address = $data['address'];
+            $newUser->save();
+            return $this->success($response, $newUser);
+
+        }catch(\Exception $e){
+            return $this->error($response, $e->getMessage());
+        }
 
     }
-
 }
+    
