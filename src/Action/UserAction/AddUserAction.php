5c5157e29bd15057504a7a6492c07886d9cd5083
Ramiz <LibraRamis@gmail.com>
2022-10-01 03:21:20 -0700

    Update AddUserAction.php

--- src/Action/UserAction/AddUserAction.php
+++ src/Action/UserAction/AddUserAction.php
@@ -2,7 +2,6 @@
 namespace App\Action\UserAction;
 
 use App\Action\Action;
-use Illuminate\Support\Facades\Hash;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use App\Models\User;
