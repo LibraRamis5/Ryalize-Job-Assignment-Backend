c2eff1344223859825a4bcc14e5958943aaa7a3a
Ramiz <LibraRamis@gmail.com>
2022-10-01 14:25:24 -0700

    fix pagination in all transactions list

--- src/Action/UserAction/GetUserTransactionAction.php
+++ src/Action/UserAction/GetUserTransactionAction.php
@@ -15,7 +15,16 @@ final class GetUserTransactionAction extends Action{
     public function __invoke(Request $request, Response $response, $args): Response {
     
         try{
+
             $requestParams = $request->getQueryParams();
+            $validation = $this->validate($requestParams, [
+                'per_page' => 'required|integer',
+                'page' => 'required|integer',
+            ]);
+
+            if($validation){
+                return $this->responce($response, $validation, 401);
+            }
 
             $transactions = ViewUserTransaction::query();
 
@@ -27,7 +36,7 @@ final class GetUserTransactionAction extends Action{
                 $transactions = $transactions->where('location_id', $requestParams['location_id']);
             }
 
-            $transactions = $transactions->paginate(50);
+            $transactions = $transactions->paginate($requestParams['per_page'], ['*'], 'page', $requestParams['page']);
             $data = ['message' => 'user transactions', 'data' => $transactions];
 
             return $this->success($response,$data);
